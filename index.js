69a619b81c97fca5e8a1618e9990f317d52aabc4
Aman Allah Rafat <aman.allah.rafat@gmail.com>
2021-01-13 01:05:07 +0200

    Test-Deployment

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const Staff_Member = require('./Models/Users/Staff_Member.js');
 const bcrypt = require('bcryptjs');
+const path = require('path');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const key = "jkanbvakljbefjkawkfew";
 
 const express = require('express');
+
+
 const hrRouter = require('./Routes/hr.js');
 const hodRouter = require('./Routes/hod.js');
-
 const staffMemberRouter = require('./Routes/staffMember.js');
 const courseInstructorRouter = require('./Routes/courseInstructor.js');
 const courseCoordinatorRouter = require('./Routes/courseCoordinator.js');
 const academicMemberRouter = require('./Routes/academicMember.js');
 
+require("dotenv").config();
+
+
 const app = express();
+
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 app.use('/', staffMemberRouter);
@@ -30,6 +41,13 @@ app.post('/init', async(req, res)=>{
     await hr.save();
     return res.send("HR added successfully");
 })
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 module.exports = app ;
 
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const { required } = require('@hapi/joi');
 const mongoose = require('mongoose');
 const app = require('./index.js');
 const periodic = require('./periodic.js');
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect("mongodb+srv://team9:team9@cluster0.j2oep.mongodb.net/gucDB?retryWrites=true&w=majority")
     .then(async() => {
-        app.listen(5000);
+        app.listen(port);
         console.log("Server is UP");
         periodic.loop_month(0);
     })
